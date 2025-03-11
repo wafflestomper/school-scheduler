01c0f4b47ee1cf080bd421c9f7876ee8340c5faa
Brian Zollinhofer <brianzollinhofer@gmail.com>
2025-03-11 17:57:46 -0400

    Add deployment configuration files

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn scheduler_config.wsgi:application 
+web: cd school_scheduler && gunicorn scheduler_config.wsgi:application --bind 0.0.0.0:$PORT --log-file - 
--- wsgi.py
+++ wsgi.py
@@ -6,6 +6,7 @@ import os
 import sys
 
 # Add the project directory to the Python path
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), 'school_scheduler'))
 
 from scheduler_config.wsgi import application 

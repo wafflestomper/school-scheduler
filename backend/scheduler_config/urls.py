18c6c0bb5bd8f890f2556384f033bf05a9e25e3a
Brian Zollinhofer <brianzollinhofer@gmail.com>
2025-03-12 16:20:35 -0400

    Fix deployment configuration and add root URL redirect

--- backend/scheduler_config/urls.py
+++ backend/scheduler_config/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/admin/', permanent=True), name='index'),
     path('admin/', admin.site.urls),
     path('scheduler/', include('scheduler.urls')),  # Include scheduler URLs with prefix
 ]

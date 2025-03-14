36ca99818f64be3c2d58f68720e11fecac194ada
Brian Zollinhofer <brianzollinhofer@gmail.com>
2025-03-14 00:57:06 -0400

    Added student schedule view to Django admin user detail page

--- backend/scheduler/admin/users.py
+++ backend/scheduler/admin/users.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from ..models import User
+from ..models import User, Period, Section
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
@@ -21,4 +21,34 @@ class UserAdmin(BaseUserAdmin):
         }),
     )
     search_fields = ('username', 'user_id', 'first_name', 'last_name', 'email')
-    ordering = ('username',) 
+    ordering = ('username',)
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        user = self.get_object(request, object_id)
+        
+        if user and user.role == 'STUDENT':
+            # Get all periods
+            periods = Period.objects.all().order_by('start_time')
+            
+            # Get student's sections grouped by period
+            student_sections = {}
+            student_sections_qs = Section.objects.filter(
+                students=user
+            ).select_related(
+                'course', 'teacher', 'room', 'period'
+            )
+            
+            for section in student_sections_qs:
+                if section.period_id not in student_sections:
+                    student_sections[section.period_id] = []
+                student_sections[section.period_id].append(section)
+            
+            extra_context.update({
+                'periods': periods,
+                'student_sections': student_sections,
+            })
+        
+        return super().change_view(
+            request, object_id, form_url, extra_context=extra_context,
+        ) 
